ad2a072878a697731f2661d4163dbb0057b142f1
Tyzer1 <Tizara1@protonmail.com>
2022-07-21 18:16:51 +0300

    Edit department button added

--- src/EditDepModal.js
+++ src/EditDepModal.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
 
-export class AddDepModal extends Component{
+export class EditDepModal extends Component{
     constructor(props){
         super(props);
         this.handleSubmit=this.handleSubmit.bind(this);
@@ -10,12 +10,13 @@ export class AddDepModal extends Component{
     handleSubmit(event){
         event.preventDefault();
         fetch(process.env.REACT_APP_API + 'department',{
-            method:'POST',
+            method:'PUT',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
+                DepartmentId:event.target.DepartmentId.value,
                 DepartmentName:event.target.DepartmentName.value
             })
         })
@@ -37,7 +38,7 @@ export class AddDepModal extends Component{
                 centered>
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title-vcenter">
-                            Add Department
+                            Edit Department
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
@@ -45,15 +46,24 @@ export class AddDepModal extends Component{
                         <Row>
                             <Col sm={6}>
                                 <Form onSubmit={this.handleSubmit}>
+                                    <Form.Group controlId="DepartmentId">
+                                        <Form.Label>DepartmentId</Form.Label>
+                                        <Form.Control type="text" name="DepartmentId" required 
+                                        disabled
+                                        defaultValue={this.props.depid}
+                                        placeholder="DepartmentId"/>
+                                    </Form.Group>
+
                                     <Form.Group controlId="DepartmentName">
                                         <Form.Label>DepartmentName</Form.Label>
-                                        <Form.Control type="text" name="DepartmentName" required 
+                                        <Form.Control type="text" name="DepartmentName" required
+                                        defaultValue={this.props.depname}
                                         placeholder="DepartmentName"/>
                                     </Form.Group>
 
                                     <Form.Group>
                                         <Button variant="primary" type="submit">
-                                            Add Department
+                                            Update Department
                                         </Button>
                                     </Form.Group>
                                 </Form>
